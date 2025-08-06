cce7ef6b6bc1914722ec789f2ab7f72c1f846ac2
Sundarampal <sundrampal61@gmail.com>
2025-08-06 16:04:13 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -21,12 +21,14 @@ class EditableTextExample extends StatefulWidget {
 }
 
 class _EditableTextExampleState extends State<EditableTextExample> {
-  int n1 = 0, n2 = 0, n3 = 0;
+
+final _formkey = GlobalKey<FormState>();
+  int n1 = 0, n2 = 0, n3 = 0, n4 = 0;
   int result = 0;
 
   void addNumbers() {
     setState(() {
-      result = n1 + n2 + n3;
+      result = n1 + n2 + n3 + n4;
     });
   }
 
@@ -36,32 +38,52 @@ class _EditableTextExampleState extends State<EditableTextExample> {
       appBar: AppBar(title: Text('calculate number'), centerTitle: true),
       body: Column(
         children: [
+Text("N1"),
           TextField(
             keyboardType: TextInputType.number,
             onChanged: (value) {
               setState(() {
                 n1 = int.tryParse(value) ?? 0;
+                addNumbers();
               });
             },
           ),
+          Text("N2"),
           TextField(
             keyboardType: TextInputType.number,
             onChanged: (value) {
               setState(() {
                 n2 = int.tryParse(value) ?? 0;
+                addNumbers();
               });
-              
+
             },
 
           ),
+          Text("N3"),
           TextField(
             keyboardType: TextInputType.number,
             onChanged: (value) {
               setState(() {
                 n3 = int.tryParse(value) ?? 0;
+                addNumbers();
               });
             },
           ),
+          Text("N4"),
+          Form(
+            key: _formkey,
+            child: TextFormField(
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                setState(() {
+                  addNumbers();
+                  n4 = int.tryParse(value) ?? 0;
+                  addNumbers();
+                });
+              },
+            ),
+          ) ,
           SizedBox(height: 20),
           ElevatedButton(onPressed: addNumbers, child: Text("Add")),
           SizedBox(height: 20),
